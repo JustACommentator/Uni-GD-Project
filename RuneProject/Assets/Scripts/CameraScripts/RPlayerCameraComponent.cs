d5f6a3ffbe5f63a599bd2b8a17dad72a7cd9a47b
Tim B <78982307+Tamagear@users.noreply.github.com>
2022-03-31 19:46:08 +0200

    RuneProject aufgesetzt

---
+++ RuneProject/Assets/Scripts/CameraScripts/RPlayerCameraComponent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RuneProject.CameraSystem
+{
+    public class RPlayerCameraComponent : MonoBehaviour
+    {
+
+    }
+}
