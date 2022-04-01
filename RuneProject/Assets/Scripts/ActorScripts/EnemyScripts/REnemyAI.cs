f8df91ce4372ba8b796d73af7cae8acae70f3688
DasPriester <michelrothboeck@gmail.com>
2022-04-01 14:04:57 +0200

    State AI

--- RuneProject/Assets/Scripts/ActorScripts/EnemyScripts/REnemyAI.cs
+++ RuneProject/Assets/Scripts/ActorScripts/EnemyScripts/REnemyAI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class REnemyMoveTo : MonoBehaviour
+public class REnemyAI : MonoBehaviour
 {
 
     [SerializeField] private Transform goal = null;
@@ -12,15 +12,61 @@ public class REnemyMoveTo : MonoBehaviour
     [Header("References")]
     [SerializeField] private Rigidbody enemyRigidbody = null;
     [SerializeField] private NavMeshAgent agent = null;
+    [SerializeField] private State currentState = State.idle;
+    [SerializeField] private float susDistance = 5;
+
+    private float susCount = 0;
+    private float agressiveness = 0.01f;
+
+    enum State
+    {
+        idle,
+        suspicious,
+        aggro
+    }
+
 
     void Update()
     {
-        agent.destination = goal.position;
-
-
-        if (Vector3.Distance(enemyRigidbody.position, goal.position) < attackDistance)
+        switch (currentState)
         {
-            //DoDamage();
+            case State.idle:
+
+                if (Vector3.Distance(enemyRigidbody.position, goal.position) < susDistance)
+                {
+                    currentState = State.suspicious;
+                }
+                break;
+
+            case State.suspicious:
+
+                if (Vector3.Distance(enemyRigidbody.position, goal.position) < susDistance)
+                {
+                    susCount += Mathf.Clamp(susCount + agressiveness * Time.deltaTime, 0, 1);
+                }
+                else
+                {
+                    susCount -= Mathf.Clamp(susCount + agressiveness * Time.deltaTime, 0, 1);
+                }
+
+                if (agressiveness == 1)
+                {
+                    currentState = State.aggro;
+                }
+                break;
+
+            case State.aggro:
+
+                agent.destination = goal.position;
+
+                if (Vector3.Distance(enemyRigidbody.position, goal.position) < attackDistance)
+                {
+                    //DoDamage();
+                }
+                break;
+
+            default:
+                break;
         }
     }
 }
