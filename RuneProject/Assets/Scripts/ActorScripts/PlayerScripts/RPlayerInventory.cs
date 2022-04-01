64b9df1144c4aa5252292c04927253cc09e2a306
Tim B <78982307+Tamagear@users.noreply.github.com>
2022-04-01 15:24:52 +0200

    Corrected Inventory, Moved Levels

--- RuneProject/Assets/Scripts/ActorScripts/PlayerScripts/RPlayerInventory.cs
+++ RuneProject/Assets/Scripts/ActorScripts/PlayerScripts/RPlayerInventory.cs
@@ -7,7 +7,12 @@ public class RPlayerInventory : MonoBehaviour
     [SerializeField] private int initialCrystals = 0;
     
     private int currentCrystals = 0;
-    
+
+    private void Start()
+    {
+        currentCrystals = initialCrystals;
+    }
+
     private void RemoveCrystals(int amount) {
         currentCrystals = currentCrystals - Mathf.Clamp(amount, -currentCrystals, 0);
     }
