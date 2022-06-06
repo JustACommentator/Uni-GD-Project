44697a994c0f3d4fa52132415fb70674c39a7e2e
DasPriester <michelrothboeck@gmail.com>
2022-06-06 16:48:41 +0200

    new room generation

--- RuneProject/Assets/Scripts/EnvironmentSystem/RMapGenerator.cs
+++ RuneProject/Assets/Scripts/EnvironmentSystem/RMapGenerator.cs
@@ -7,11 +7,9 @@ namespace RuneProject.EnvironmentSystem
     enum TileType
     {
         WALL,
-        EMPTY,
-        CORNER,
-        EDGE,
-        PILLAR,
-        DOOR
+        FLOOR,
+        VOID,
+        PILLAR
     }
 
     enum Orientation
@@ -22,10 +20,16 @@ namespace RuneProject.EnvironmentSystem
         WEST
     }
 
+    struct RoomLayout
+    {
+        public KeyValuePair<TileType, Orientation>[][] room;
+        public bool[] dooramp;
+    }
+
     public class RMapGenerator : MonoBehaviour
     {
-        private KeyValuePair<TileType, Orientation>[][] tileMap;
-        [SerializeField] private Vector2 tileSize;
+        private List<RoomLayout> roomLayouts;
+        private List<GameObject> rooms;
         [SerializeField] private GameObject[] tileObjects;
 
         [Header("Enemies")]
@@ -33,37 +37,78 @@ namespace RuneProject.EnvironmentSystem
         [SerializeField] private GameObject fairy;
 
         private Dictionary<char, Vector2> waypoints = new Dictionary<char, Vector2>();
-
-        internal KeyValuePair<TileType, Orientation>[][] TileMap { get => tileMap; set => tileMap = value; }
+        private Vector2 tileSize;
 
         void Start()
         {
-            tileMap = tileMapFromString(
-                "||||||||||||||||||||#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|                  |#" +
-                "|    1        2    |#" +
-                "|                  |#" +
-                "||||||        ||||||#");
+            int seed = 1234;
 
-            string enemyLayout =
+            Random.InitState(seed);
+
+            tileSize = new Vector2(1, 1);
+            roomLayouts = new List<RoomLayout>();
+            roomLayouts.Add(roomLayoutFromString(
+                "|||  |||#" +
+                "|.    .|#" +
+                "  1     #" +
+                "     2  #" +
+                "|.    .|#" +
+                "|||  |||#")
+            );
+
+            string[] enemyLayouts =
+            {
                 "S1,2#" +
-                "S2,1#";
+                "S2,1#",
+            };
 
+            RoomLayout rootRoom = roomLayouts[Random.Range(0, roomLayouts.Count)];
+
+            string mapLayout =
+                "0#";
+
+
+            int rx = 0, ry = 7;
+            foreach (char c in mapLayout)
+            {
+                if (c == '#')
+                {
+                    rx = 0;
+                    ry--;
+                } else
+                {
+                    GameObject room = Instantiate(new GameObject(), transform);
+                    room.name = "Room " + rx + ":" + ry;
+
+                    GenerateEnemies(enemyLayouts[int.Parse(c.ToString())], rx, ry, room.transform);
+
+                    GenerateMap(roomLayouts[int.Parse(c.ToString())].room, rx, ry, room.transform);
+                    rx++;
+                }
+            }
+        }
+
+        private void GenerateMap(KeyValuePair<TileType, Orientation>[][] tileMap, float room_x, float room_y, Transform parent)
+        {
+            for (int y = 0; y < tileMap.Length; y++)
+            {
+                for (int x = 0; x < tileMap[y].Length; x++)
+                {
+                    if (tileMap[y][x].Key != TileType.FLOOR)
+                    {
+                        GameObject tile = Instantiate(
+                            GetGameObject(tileMap[y][x].Key),
+                            tilePosition(x, y, room_x, room_y),
+                            GetQuaternion(tileMap[y][x].Value),
+                            parent);
+                        tile.name = "Tile " + tileMap[y][x].Key;
+                    }
+                }
+            }
+        }
+
+        private void GenerateEnemies(string enemyLayout, float room_x, float room_y, Transform parent)
+        {
             GameObject enemyType = null;
             char idx = ' ';
             List<Transform> path = new List<Transform>();
@@ -85,17 +130,22 @@ namespace RuneProject.EnvironmentSystem
                         Vector2 xy = waypoints[idx];
                         if (!instantiated.ContainsKey(idx))
                         {
-                            waypoint = Instantiate(new GameObject(), tilePosition(xy.x,xy.y) + Vector3.up, new Quaternion(), transform).transform;
+                            GameObject wp = Instantiate(new GameObject(), tilePosition(xy.x, xy.y, room_x, room_y) + Vector3.up, new Quaternion(), parent);
+                            wp.name = "Waypoint " + xy.x + ":" + xy.y;
+
+                            waypoint = wp.transform;
                             instantiated.Add(idx, waypoint);
-                        } else
+                        }
+                        else
                         {
                             waypoint = instantiated[idx];
                         }
                         path.Add(waypoint);
                         if (c == '#')
                         {
-                            GameObject enemy = Instantiate(enemyType, path[0].position, new Quaternion(), transform);
+                            GameObject enemy = Instantiate(enemyType, path[0].position, new Quaternion(), parent);
                             enemy.GetComponent<EnemySystem.REnemyAI>().Path = path;
+                            enemy.name = enemyType.name;
                             path = new List<Transform>();
                         }
                         break;
@@ -103,24 +153,6 @@ namespace RuneProject.EnvironmentSystem
                         idx = c;
                         break;
 
-                }
-            }
-
-            GenerateMap();
-        }
-
-        private void GenerateMap()
-        {
-            for (int y = 0; y < tileMap.Length; y++)
-            {
-                for (int x = 0; x < tileMap[y].Length; x++)
-                {
-                    if (TileMap[x][y].Key != TileType.EMPTY)
-                        Instantiate(
-                            GetGameObject(TileMap[x][y].Key),
-                            tilePosition(x, y),
-                            GetQuaternion(TileMap[x][y].Value),
-                            transform);
                 }
             }
         }
@@ -131,11 +163,11 @@ namespace RuneProject.EnvironmentSystem
             {
                 case TileType.WALL:
                     return tileObjects[0];
-                case TileType.CORNER:
+                case TileType.FLOOR:
                     return tileObjects[1];
-                case TileType.PILLAR:
+                case TileType.VOID:
                     return tileObjects[2];
-                case TileType.DOOR:
+                case TileType.PILLAR:
                     return tileObjects[3];
                 default:
                     return null;
@@ -160,7 +192,7 @@ namespace RuneProject.EnvironmentSystem
             return quat;
         }
 
-        private KeyValuePair<TileType, Orientation>[][] tileMapFromString(string map)
+        private RoomLayout roomLayoutFromString(string map)
         {
 
             List<TileType[]> lookup_temp = new List<TileType[]>();
@@ -172,13 +204,13 @@ namespace RuneProject.EnvironmentSystem
                 switch (c)
                 {
                     case ' ':
-                        lookup_row.Add(TileType.EMPTY);
+                        lookup_row.Add(TileType.FLOOR);
                         break;
                     case '|':
                         lookup_row.Add(TileType.WALL);
                         break;
-                    case ':':
-                        lookup_row.Add(TileType.DOOR);
+                    case '.':
+                        lookup_row.Add(TileType.VOID);
                         break;
                     case '#':
                         lookup_temp.Add(lookup_row.ToArray());
@@ -188,7 +220,7 @@ namespace RuneProject.EnvironmentSystem
                         break;
                     default:
                         waypoints.Add(c, new Vector2(ix, iy));
-                        lookup_row.Add(TileType.EMPTY);
+                        lookup_row.Add(TileType.FLOOR);
                         break;
                 }
                 ix++;
@@ -199,12 +231,11 @@ namespace RuneProject.EnvironmentSystem
             Dictionary<char, Vector2> rwayp = new Dictionary<char, Vector2>();
             foreach (KeyValuePair<char, Vector2> wp in waypoints)
             {
-                rwayp[wp.Key] = new Vector2(wp.Value.x, lookup_temp.Count - wp.Value.y);
+                rwayp[wp.Key] = new Vector2(wp.Value.x, lookup_temp.Count - 1 - wp.Value.y);
             }
             waypoints = rwayp;
 
             TileType[][] lookup = lookup_temp.ToArray();
-
 
             KeyValuePair<TileType, Orientation>[][] o = new KeyValuePair<TileType, Orientation>[lookup.Length][];
 
@@ -213,71 +244,57 @@ namespace RuneProject.EnvironmentSystem
                 List<KeyValuePair<TileType, Orientation>> row = new List<KeyValuePair<TileType, Orientation>>();
                 for (int x = 0; x < lookup[y].Length; x++)
                 {
-                    switch (lookup[x][y])
+                    switch (lookup[y][x])
                     {
                         case TileType.WALL:
-                        case TileType.DOOR:
                             bool cat = false;
 
-                            bool open_n = y != 0;
-                            bool wall_n = open_n && (lookup[x][y - 1] == TileType.WALL || lookup[x][y - 1] == TileType.DOOR);
-                            bool open_e = x != lookup.Length - 1;
-                            bool wall_e = open_e && (lookup[x + 1][y] == TileType.WALL || lookup[x + 1][y] == TileType.DOOR);
-                            bool open_s = y != lookup[x].Length - 1;
-                            bool wall_s = open_s && (lookup[x][y + 1] == TileType.WALL || lookup[x][y + 1] == TileType.DOOR);
-                            bool open_w = x != 0;
-                            bool wall_w = open_w && (lookup[x - 1][y] == TileType.WALL || lookup[x - 1][y] == TileType.DOOR);
+                            bool open_n = x != 0;
+                            bool wall_n = open_n && (lookup[y][x - 1] == TileType.WALL);
+                            bool open_e = y != lookup.Length - 1;
+                            bool wall_e = open_e && (lookup[y + 1][x] == TileType.WALL);
+                            bool open_s = x != lookup[y].Length - 1;
+                            bool wall_s = open_s && (lookup[y][x + 1] == TileType.WALL);
+                            bool open_w = y != 0;
+                            bool wall_w = open_w && (lookup[y - 1][x] == TileType.WALL);
 
-                            if (lookup[x][y] == TileType.WALL)
-                            {
-                                if (!wall_n && !wall_e && !wall_s && !wall_w) {
-                                    row.Add(new KeyValuePair<TileType, Orientation>(TileType.PILLAR, Orientation.NORTH));
-                                    cat = true;
-                                }
-
-                                if (wall_n && wall_e) {
-                                    row.Add(new KeyValuePair<TileType, Orientation>(TileType.CORNER, Orientation.SOUTH));
-                                    cat = true;
-                                }
-                                if (wall_e && wall_s) {
-                                    row.Add(new KeyValuePair<TileType, Orientation>(TileType.CORNER, Orientation.WEST));
-                                    cat = true;
-                                }
-                                if (wall_s && wall_w) {
-                                    row.Add(new KeyValuePair<TileType, Orientation>(TileType.CORNER, Orientation.NORTH));
-                                    cat = true;
-                                }
-                                if (wall_w && wall_n) {
-                                    row.Add(new KeyValuePair<TileType, Orientation>(TileType.CORNER, Orientation.EAST));
-                                    cat = true;
-                                }
+                            if (lookup[y][x] == TileType.WALL &&!wall_n && !wall_e && !wall_s && !wall_w) {
+                                row.Add(new KeyValuePair<TileType, Orientation>(TileType.PILLAR, Orientation.NORTH));
+                                cat = true;
                             }
 
                             if (!cat && (wall_n || wall_s)) {
-                                row.Add(new KeyValuePair<TileType, Orientation>(lookup[x][y], Orientation.EAST));
+                                row.Add(new KeyValuePair<TileType, Orientation>(lookup[y][x], Orientation.EAST));
                                 cat = true;
                             }
                             if (!cat && (wall_e || wall_w)) {
-                                row.Add(new KeyValuePair<TileType, Orientation>(lookup[x][y], Orientation.NORTH));
+                                row.Add(new KeyValuePair<TileType, Orientation>(lookup[y][x], Orientation.NORTH));
                                 cat = true;
                             }
                             if (!cat) row.Add(new KeyValuePair<TileType, Orientation>(TileType.PILLAR, Orientation.NORTH));
                             break;
 
-                        case TileType.EMPTY:
-                            row.Add(new KeyValuePair<TileType, Orientation>(TileType.EMPTY, Orientation.NORTH));
+                        case TileType.FLOOR:
+                            row.Add(new KeyValuePair<TileType, Orientation>(TileType.FLOOR, Orientation.NORTH));
+                            break;
+                        case TileType.VOID:
+                            row.Add(new KeyValuePair<TileType, Orientation>(TileType.VOID, Orientation.NORTH));
                             break;
                     }
                 }
                 o[y] = row.ToArray();
             }
 
-            return o;
+            RoomLayout layout = new RoomLayout();
+            layout.room = o;
+            layout.dooramp = new bool[4] { o[5][3].Key == TileType.FLOOR, o[2][7].Key == TileType.FLOOR, o[0][3].Key == TileType.FLOOR, o[2][0].Key == TileType.FLOOR };
+
+            return layout;
         }
 
-        Vector3 tilePosition(float x, float y)
+        Vector3 tilePosition(float x, float y, float room_x, float room_y)
         {
-            return new Vector3(transform.position.x + (x + 0.5f) * tileSize.x, transform.position.y, transform.position.z + (y + 0.5f) * tileSize.y);
+            return new Vector3(transform.position.x + (x + room_x * 8 + 0.5f) * tileSize.x, transform.position.y, transform.position.z + (y + room_y * 6 + 0.5f) * tileSize.y);
         }
     }
 }
