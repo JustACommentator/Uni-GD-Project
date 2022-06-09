7300d8f2502e65b0462a3d3c03e4c8c7bbbf431c
Tim B <78982307+Tamagear@users.noreply.github.com>
2022-06-09 04:13:36 +0200

    Player Pause (ESC)

--- RuneProject/Assets/Scripts/UserInterfaceSystem/RUI_Main.cs
+++ RuneProject/Assets/Scripts/UserInterfaceSystem/RUI_Main.cs
@@ -11,26 +11,66 @@ namespace RuneProject.UserInterfaceSystem
         [Header("References")]
         [SerializeField] private RPlayerMovement playerMovement = null;
         [SerializeField] private RPlayerSoundHandler soundHandler = null;
+        [SerializeField] private RPlayerHealth playerHealth = null;
         [Space]
         [SerializeField] private GameObject endOfVSliceParent = null;
+        [SerializeField] private GameObject pauseParent = null;
         [Space]
         [SerializeField] private RLevelTransition loadingScreenPrefab = null;
 
         private bool isWaitingForEndOfVSliceInput = false;
         private bool blockPlayerPause = false;
         private bool isLoading = false;
+        private bool isPaused = false;
+        private Coroutine currentPauseRoutine = null;
 
         public event System.EventHandler OnReachEnd;
 
         private const string MENU_LEVEL = "MainMenu";
 
+        private void Start()
+        {
+            playerHealth.OnDeath += PlayerHealth_OnDeath;
+        }
+
+        private void PlayerHealth_OnDeath(object sender, GameObject e)
+        {
+            blockPlayerPause = true;
+        }
+
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape) && isWaitingForEndOfVSliceInput)
+            if (Input.GetKeyDown(KeyCode.Escape)) 
             {
-                LoadScene(MENU_LEVEL);   
+                if (isWaitingForEndOfVSliceInput)
+                    LoadScene(MENU_LEVEL);
+                else
+                    Pause();
             }
         }
+
+        private void Pause()
+        {
+            if (isPaused || blockPlayerPause) return;
+
+            pauseParent.SetActive(true);
+            isPaused = true;
+            Time.timeScale = 0f;
+
+            if (currentPauseRoutine != null)
+                StopCoroutine(currentPauseRoutine);
+
+            currentPauseRoutine = StartCoroutine(IPauseUpdate());
+        }
+
+        private void UnPause()
+        {
+            if (!isPaused) return;
+
+            Time.timeScale = 1f;
+            pauseParent.SetActive(false);
+            isPaused = false;
+        }       
 
         public void LoadScene(string levelName)
         {
@@ -49,6 +89,19 @@ namespace RuneProject.UserInterfaceSystem
             playerMovement.BlockMovementInput();
             endOfVSliceParent.SetActive(true);
             OnReachEnd?.Invoke(this, null);
+        }
+
+        private IEnumerator IPauseUpdate()
+        {
+            yield return new WaitForSecondsRealtime(0.0016f);
+
+            while (isPaused)
+            {
+                if (Input.GetKeyDown(KeyCode.Escape))                
+                    UnPause();
+                
+                yield return new WaitForSecondsRealtime(0.00016f);
+            }
         }
     }
 }
